219d7f0b324119cb972ef877ba1ae16b469bac4e
Yonas Gebrehiwot <yonihabeshaw@gmail.com>
2024-10-05 00:23:29 +0300

    0-promise.js

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,8 @@
+export default function getResponseFromAPI() {
+  return new Promise(() => {
+    // return new Promise(function(resolve, reject) {} works too
+    // fetch something e.g data and make a request
+    // if it is a success call the resolve function and pass data
+    // if there is an error call reject and pass the error 
+  });
+}

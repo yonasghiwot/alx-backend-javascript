4fbbbdbde6be3502a8b1d814c83961e8316144e0
Yonas Gebrehiwot <yonihabeshaw@gmail.com>
2024-10-13 17:58:28 +0300

    9-groceries_list.js

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,15 @@
+/**
+ * Returns Map of groceries with their quantities.
+ *
+ * @return {Map} Map of groceries with their quantities
+ */
+export default function groceriesList() {
+  const values = [
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ];
+  return new Map(values);
+}

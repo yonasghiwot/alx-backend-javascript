c1e216f68ffe525a6fda1f803a82a8ac51c3b525
Yonas Gebrehiwot <yonihabeshaw@gmail.com>
2024-11-26 23:57:47 +0300

    0-console.js

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Prints the given message to standardoutput.
+ *
+ * @param {msg} str - The message to be printed.
+ */
+function displayMessage(msg) {
+  console.log(msg);
+}
+
+module.exports = displayMessage;
